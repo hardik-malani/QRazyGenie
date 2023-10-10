fa1d31b0ce74f4db749472d4dfe165363f26bb73
The-GO0d-Guy <malaha814@gmail.com>
2023-10-10 20:15:11 +0530

    update-folder-structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import { QRCode } from 'react-qrcode-logo';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Sidebar from './components/Sidebar';
 
 function App() {
 
   return (
     <>
+    <div className='flex flex-row'>
+    <Sidebar/>
+    <div>
     <Navbar/>
     <Hero/>
     <Footer/>
 <QRCode value="https://github.com/gcoro/react-qrcode-logo" bgColor='yellow' quietZone={10} fgColor="green" logoImage={"/Groot.jpg"} logoWidth="40" logoOpacity="9" qrStyle="square" />,
+
+    </div>
+    </div>
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <div className='h-[46rem] w-[10em] bg-gray-400'>
+        A
+    </div>
+  )
+}
