c0801267c990dee76ae9d5490e11a660f6d83322
The-GO0d-Guy <malaha814@gmail.com>
2023-10-12 23:44:19 +0530

    minor-icon-colour-changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <>
-      <div className="flex flex-col overflow-x-hidden h-screen">
+      <div className="flex flex-col overflow-x-hidden h-screen bg-gradient-to-r from-green-300 via-blue-400 to-purple-600">
         <Navbar />
         <div className="flex flex-row">
           <Sidebar />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { FaCloudUploadAlt, FaBars, FaTimes, FaPalette } from "react-icons/fa";
 
 export default function Sidebar() {
   return (
-    <div className='h-[46rem] w-[10em] bg-gray-400 hidden md:block'>
-        A
+    <div className="h-[46rem] w-[5em] p-3 space-y-5 bg-gray-400 hidden md:block">
+
+        <button>
+          <FaTimes size={32} className="hover:text-gray-700"/>
+          <p>Close</p>
+        </button>
+      <button>
+        <FaCloudUploadAlt size={48} className="hover:text-gray-700"/>
+        <p>Logo</p>
+      </button>
+
+      <button>
+        <FaPalette size={32} className="hover:text-gray-700"/>
+        <p>Color</p>
+      </button>
     </div>
-  )
+  );
 }
