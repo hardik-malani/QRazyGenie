950d67658aeaaf8919924d1a054caf1b61774314
The-GO0d-Guy <malaha814@gmail.com>
2023-10-14 23:58:35 +0530

    navbar-update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='bg-[#013220] w-screen p-3'>
+    <div className='bg-[#013220] w-screen p-4 text-yellow-400'>
       Navbar
       </div>
   )
