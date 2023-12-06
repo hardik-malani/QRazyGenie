5f6c20e411bb8776159816155af7c52f6e92f4dc
The-GO0d-Guy <malaha814@gmail.com>
2023-12-06 06:59:34 +0530

    about-page-update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import ReactPlayer from 'react-player/youtube'
+
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="bg-gray-300 h-screen min-h-[100%] w-full flex flex-col justify-center items-center space-y-10">
+      <span className="text-2xl font-semibold underline">Video Demo</span>
+      <ReactPlayer url='https://youtu.be/AispB2upUks?si=Q9LgAvt0qO5dje8C' controls='true' />
+    </div>
+  );
 }
